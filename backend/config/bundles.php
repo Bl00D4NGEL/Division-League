a80943426ffa2939c89f172a634b16ef83f8f9f1
Bl00D4NGEL <kuhlesdominik@gmx.de>
2019-09-27 18:27:51 +0200

    added deploy bundle

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
 ];
