cfce23b30dee3a1593ba8c1934079b5cf1672d1e
Bl00D4NGEL <kuhlesdominik@gmx.de>
2019-10-01 20:19:22 +0200

    corb fix?

--- backend/src/Resource/JsonResponse/SuccessResponse.php
+++ backend/src/Resource/JsonResponse/SuccessResponse.php
@@ -3,11 +3,13 @@
 namespace App\Resource\JsonResponse;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class SuccessResponse extends JsonResponse
 {
-    public function __construct($data = null, int $status = 200, array $headers = [])
+    public function __construct($data = null, int $status = Response::HTTP_OK, array $headers = [])
     {
+        $headers[] = "X-Content-Type-Options: nosniff";
         parent::__construct(["status" => "success", "data" => $data], $status, $headers);
     }
 }
