5631139dde2f2f9d266cc272dc71976d244cd218
Dominik Peters <gitkraken@d-peters.com>
2019-12-07 18:53:22 +0100

    changed footer link

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from "react";
 import './footer.scss';
 
 export default function Footer () {
-    return <footer>Made by <a target="_blank" href="https://di.community/profile/32323-bl00d4ngel/">Bl00D4NGEL</a></footer>
+    return <footer>Made by <a target="_blank" href="https://dmginc.gg/profile/70-bl00d4ngel/">Bl00D4NGEL</a></footer>
 }
