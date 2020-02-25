8babd561b29efb5d93a9c2dff0ca448b19635cbf
Bl00D4NGEL <kuhlesdominik@gmx.de>
2020-02-25 07:43:11 +0100

    added explanation for adding a player

--- frontend/src/components/AddPlayer/AddPlayerForm.js
+++ frontend/src/components/AddPlayer/AddPlayerForm.js
@@ -54,6 +54,16 @@ export default function AddPlayerForm() {
     };
 
     return <div>
+        <div>
+            What do the following fields mean?
+            Given the example member Bl00D4NGEL who's member page can be found at <a href="https://dmginc.gg/profile/70-bl00d4ngel/">https://dmginc.gg/profile/70-bl00d4ngel/</a> we can extract the following info:<br />
+            <ul>
+                <li>Name: Bl00D4NGEL</li>
+                <li>Division: XIX</li>
+                <li>Player ID: 70 (In the url the number after profile/...)</li>
+                <li>League: Well you gotta figure that out yourself</li>
+            </ul>
+        </div>
         <CustomForm
             onSubmit={handleSubmit}
             formFields={
